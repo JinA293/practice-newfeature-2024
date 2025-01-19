7fdec29e622a9cc7d77485a09f7f6da30c047a0d
JinA293 <jin.ha81217@gmail.com>
2025-01-19 13:20:54 +0900

    relativeColor見やすく

--- data/section/relativeColor.ts
+++ data/section/relativeColor.ts
@@ -1,38 +1,42 @@
 export const relativeColor = {
   title: "relativecolor",
   htmlCode: `
-<div class="origin-color">
-  <code>blue</code>
-</div>
-<div class="relative-color">
-  <code>oklch(from var(--originColor) calc(l - .25) c h)</code>
-</div>
+    <div class="origin-color">
+      <code>origin</code>
+    </div>
+    <div class="relative-color">
+      <code>relative</code>
+    </div>
   `,
   cssCode: `
-:root {
-  --originColor: blue;
-}
-@layer demo {
-  .origin-color {
-    background: var(--originColor);
-  }
-
-    .relative-color {
-    background: oklch(from var(--originColor) calc(l - .25) c h);
-  }
-} 
-  .origin-color {
-    color: white;
-  }
-  
-  .relative-color {
-    color: black;
-    
-    & > code {
-      background: white;
-      border-radius: 100px;
+    :root {
+      --originColor: blue;
     }
-  }
-}
+    @layer demo {
+      .origin-color {
+        background: var(--originColor);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .relative-color {
+        background: oklch(from var(--originColor) calc(l - .25) c h);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    } 
+    .origin-color {
+      color: white;
+      height: 75px;
+      font-size: 5em;
+    }
+    
+    .relative-color {
+      color: white;
+      height: 75px;
+      font-size: 5em;
+    }
   `,
 };
