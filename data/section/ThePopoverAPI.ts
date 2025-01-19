cfbe210e01f13db2e7c68a2e7a7581c6d3ff558c
JinA293 <jin.ha81217@gmail.com>
2025-01-19 14:06:22 +0900

    popoverAPI見やすく

--- data/section/ThePopoverAPI.ts
+++ data/section/ThePopoverAPI.ts
@@ -2,40 +2,51 @@ export const ThePopoverAPI = {
   title: "ThePopoverAPI",
   htmlCode: `
     <div class="ThePopoverAPI">
-      <button popovertarget="my-popover">Open Popover</button>
+      <button popovertarget="my-popover">おしてー</button>
       <div id="my-popover" popover>
-        <p>I am a popover with more information. Hit <kbd>esc</kbd> or click away to close me.</p>
+        <p>こんにちわー</p>
       </div>
     </div>
   `,
   cssCode: `
+    .ThePopoverAPI {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 150px;
+    }
+
+
     .ThePopoverAPI button {
-      font-size: 100%;
-      padding: 0.75rem;
-      background: white;
-      transition-duration: 0.5s;
-      border: 2px solid #ccc;
+      font-size: 1.5rem;
+      padding: 1rem 2rem;
+      background: #ff6f61;
+      color: white;
+      border: none;
       border-radius: 0.5rem;
+      cursor: pointer;
+      transition: transform 0.3s;
     }
 
     .ThePopoverAPI button:hover,
     .ThePopoverAPI button:focus {
-      background: #ccc;
-      color: black;
+      background: #ff6f61;
+      transform: scale(1.05);
     }
 
     .ThePopoverAPI [popover] {
-      background: #333;
+      background:rgb(134, 97, 255);
       color: white;
       font-weight: 400;
       padding: 1rem 1.5rem;
       border-radius: 0.5rem;
-      max-width: 20ch;
-      line-height: 1.4;
       top: 2rem;
       margin: 0 auto;
       transition: translate 0.7s ease-out, display 0.7s ease-out allow-discrete, overlay 0.7s ease-out allow-discrete;
       translate: 0 -22rem;
+      max-width: 25ch;
+      line-height: 1.6;
+      font-size: 1.5rem;
     }
 
     .ThePopoverAPI [popover]:popover-open {
